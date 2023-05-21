0d17dfe78c12191ab783c73d9a681951c6272cbd
XK <47260768+XK9274@users.noreply.github.com>
2023-05-21 17:47:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
-
 int main(int argc, char *argv[]) {
-    if (argc != 6) {
-        printf("Usage: %s duration image_path x_position y_position resize_percentage\n", argv[0]);
+    if (argc != 5) {
+        printf("Usage: %s duration image_path x_position y_position\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -46,30 +44,17 @@ int main(int argc, char *argv[]) {
     void* fb0_map = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
     SDL_Surface* screen = SDL_CreateRGBSurfaceFrom(fb0_map, width, height, bpp, pitch, 0, 0, 0, 0);
 
-    int resize_percentage = atoi(argv[5]);
-    int target_width = width * resize_percentage / 100;
-    int target_height = height * resize_percentage / 100;
-
     SDL_Surface* image = IMG_Load(argv[2]);
     if (!image) {
         printf("Error: Unable to load image: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    double width_ratio = (double)target_width / image->w;
-    double height_ratio = (double)target_height / image->h;
-    double resize_ratio = (width_ratio < height_ratio) ? width_ratio : height_ratio;
-
-    int resized_width = image->w * resize_ratio;
-    int resized_height = image->h * resize_ratio;
-
-   SDL_Surface* resized_image = zoomSurface(image, resize_ratio, resize_ratio, 1);
-    SDL_FreeSurface(image);
-    image = resized_image;
-
-    // Rotate the image
     double angle = 180.0;
-    SDL_Surface* rotated_image = rotozoomSurface(image, angle, 1.0, 1);
+    double zoom = 1.0;
+    int smooth = 1;
+    SDL_Surface* rotated_image = rotozoomSurface(image, angle, zoom, smooth);
+
     SDL_FreeSurface(image);
     image = rotated_image;
 
@@ -79,8 +64,8 @@ int main(int argc, char *argv[]) {
     SDL_Rect dst_rect = {
         .x = x_position,
         .y = y_position,
-        .w = resized_width,
-        .h = resized_height
+        .w = image->w,
+        .h = image->h
     };
 
     Uint32 start_time = SDL_GetTicks();
