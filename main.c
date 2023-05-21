8f4824d39bf3c0b0b2cb7818db1a737ca07c60fe
XK <47260768+XK9274@users.noreply.github.com>
2023-05-21 17:57:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        printf("Usage: %s duration image_path x_position y_position\n", argv[0]);
+    if (argc != 6) {
+        printf("Usage: %s duration image_path x_position y_position resize_percentage\n", argv[0]);
         return EXIT_FAILURE;
     }
 
@@ -44,17 +46,27 @@ int main(int argc, char *argv[]) {
     void* fb0_map = mmap(NULL, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, 0);
     SDL_Surface* screen = SDL_CreateRGBSurfaceFrom(fb0_map, width, height, bpp, pitch, 0, 0, 0, 0);
 
-    SDL_Surface* image = IMG_Load(argv[2]);
-    if (!image) {
-        printf("Error: Unable to load image: %s\n", SDL_GetError());
-        return EXIT_FAILURE;
-    }
+    int resize_percentage = atoi(argv[5]);
 
+	SDL_Surface* image = IMG_Load(argv[2]);
+	if (!image) {
+		printf("Error: Unable to load image: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	double resize_ratio = resize_percentage / 100.0;
+
+	int resized_width = image->w * resize_ratio;
+	int resized_height = image->h * resize_ratio;
+
+	SDL_Surface* resized_image = zoomSurface(image, resize_ratio, resize_ratio, 1);
+	SDL_FreeSurface(image);
+	image = resized_image;
+
+
+    // Rotate the image
     double angle = 180.0;
-    double zoom = 1.0;
-    int smooth = 1;
-    SDL_Surface* rotated_image = rotozoomSurface(image, angle, zoom, smooth);
-
+    SDL_Surface* rotated_image = rotozoomSurface(image, angle, 1.0, 1);
     SDL_FreeSurface(image);
     image = rotated_image;
 
@@ -64,8 +76,8 @@ int main(int argc, char *argv[]) {
     SDL_Rect dst_rect = {
         .x = x_position,
         .y = y_position,
-        .w = image->w,
-        .h = image->h
+        .w = resized_width,
+        .h = resized_height
     };
 
     Uint32 start_time = SDL_GetTicks();
